9f24e703a897ab8f173d59ce07a360bae43eb040
Almaas <almaaskn@gmail.com>
2023-07-01 17:10:20 +0530

    Initial commit

---
+++ src/products.js
@@ -0,0 +1,52 @@
+import product1 from './assets/1.jpg'
+import product2 from './assets/2.jpg'
+import product3 from './assets/3.jpg'
+import product4 from './assets/4.jpg'
+import product5 from './assets/5.jpg'
+import product6 from './assets/6.jpg'
+import product7 from './assets/7.jpg'
+
+export const PRODUCTS = [
+{
+    id: 1,
+    productName: "The Great Gatsby",
+    price: 399.0,
+    productImage: product1,
+},
+{
+    id: 2,
+    productName: "1984",
+    price: 250.0,
+    productImage: product2,
+},
+{
+    id: 3,
+    productName: "Oliver Twist",
+    price: 550.0,
+    productImage: product3,
+},
+{
+    id: 4,
+    productName: "Conversations With Friends",
+    price: 200.0,
+    productImage: product4,
+},
+{
+    id: 5,
+    productName: "The Bell Jar",
+    price: 150.0,
+    productImage: product5,
+},
+{
+    id: 6,
+    productName: "The Stranger",
+    price: 99.0,
+    productImage: product6,
+},
+{
+    id: 7,
+    productName: "Metamorphosis",
+    price: 200.0,
+    productImage: product7,
+},
+];
